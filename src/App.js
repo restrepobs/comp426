f6b63c1943ffc5c650f957dbcbb0240fe3dd1046
Amanda Harris <amandaharris@Amandas-MacBook-Pro-4.local>
2020-10-05 14:19:45 -0400

    queried results from recipe API for onions and garlic then printed response to console. This is an example of the syntax we can use to fetch data

--- src/App.js
+++ src/App.js
@@ -4,7 +4,20 @@ import { Home } from "./pages/Home.js";
 import { Navbar } from "./components/Navbar/Navbar.js";
 
 function App() {
+  fetch("http://www.recipepuppy.com/api/?i=onions,garlic&q=omelet&p=3", {
+    mode: 'no-cors',
+  })
+
+
+  .then(response => {
+	  console.log(response);
+  })
+  .catch(err => {
+	  console.log(err);
+  });
   return <Navbar />;
 }
+
+
 
 export default App;
