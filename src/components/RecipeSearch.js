1460a25d90ba2d23b2b0cfb47a2efaafb4a40bf3
restrepobs <brian@viulogix.com>
2020-11-04 00:13:29 -0500

    added recipe url

--- src/components/RecipeSearch.js
+++ src/components/RecipeSearch.js
@@ -8,18 +8,13 @@ export default class RecipeSearch extends Component {
                 <div className="container">
                     <div className="row">
                         <div className="col-10 mx-auto md-8 mt-5 text-center">
-<<<<<<< HEAD
-                            <br></br>
-                            <h1 className="text-capitalize">search for recipe with ingredients</h1>
-=======
                             <h1 className="text-capitalize" style={{marginTop:"10vh" }}>search for recipes with ingredients!</h1>
->>>>>>> 3c6bcf2b6b65600d1d3ad793c289d879dc097e0d
                             <form classname="mt-4" onSubmit={handleSubmit}>
                                 <label htmlFor="search" className="text-capitalize">
                                     type ingredients separated by comma
                                 </label>
                                 <div className="input-group">
-                                    <input type="text" name="serch" placeholder="ex:chicken,onions,carrots" 
+                                    <input type="text" name="serch" placeholder="ex: chicken,onions,carrots" 
                                     className="form-control"
                                     value={value}
                                     onChange={handleChange}
--- src/tempDetails.js
+++ src/tempDetails.js
@@ -1,158 +1,197 @@
 export const recipe = {
-        "ingredients": [
-            {
-                "name": "blueberries",
-                "image": "blueberries.jpg",
-                "amount": {
-                    "metric": {
-                        "value": 222.0,
-                        "unit": "g"
-                    },
-                    "us": {
-                        "value": 1.5,
-                        "unit": "cups"
-                    }
-                }
-            },
-            {
-                "name": "egg white",
-                "image": "egg-white.jpg",
-                "amount": {
-                    "metric": {
-                        "value": 1.0,
-                        "unit": ""
-                    },
-                    "us": {
-                        "value": 1.0,
-                        "unit": ""
-                    }
-                }
-            },
-            {
-                "name": "flour",
-                "image": "flour.png",
-                "amount": {
-                    "metric": {
-                        "value": 2.0,
-                        "unit": "Tbsps"
-                    },
-                    "us": {
-                        "value": 2.0,
-                        "unit": "Tbsps"
-                    }
-                }
-            },
-            {
-                "name": "granulated sugar",
-                "image": "sugar-in-bowl.png",
-                "amount": {
-                    "metric": {
-                        "value": 150.0,
-                        "unit": "g"
-                    },
-                    "us": {
-                        "value": 0.75,
-                        "unit": "cup"
-                    }
-                }
-            },
-            {
-                "name": "fresh lemon juice",
-                "image": "lemon-juice.jpg",
-                "amount": {
-                    "metric": {
-                        "value": 1.0,
-                        "unit": "tsp"
-                    },
-                    "us": {
-                        "value": 1.0,
-                        "unit": "tsp"
-                    }
-                }
-            },
-            {
-                "name": "nutmeg",
-                "image": "ground-nutmeg.jpg",
-                "amount": {
-                    "metric": {
-                        "value": 1.0,
-                        "unit": "pinch"
-                    },
-                    "us": {
-                        "value": 1.0,
-                        "unit": "pinch"
-                    }
-                }
-            },
-            {
-                "name": "pie dough round",
-                "image": "pie-crust.jpg",
-                "amount": {
-                    "metric": {
-                        "value": 2.0,
-                        "unit": ""
-                    },
-                    "us": {
-                        "value": 2.0,
-                        "unit": ""
-                    }
-                }
-            },
-            {
-                "name": "quick cooking tapioca",
-                "image": "tapioca-pearls.png",
-                "amount": {
-                    "metric": {
-                        "value": 2.0,
-                        "unit": "Tbsps"
-                    },
-                    "us": {
-                        "value": 2.0,
-                        "unit": "Tbsps"
-                    }
-                }
-            },
-            {
-                "name": "trimmed rhubarb",
-                "image": "rhubarb.jpg",
-                "amount": {
-                    "metric": {
-                        "value": 305.0,
-                        "unit": "g"
-                    },
-                    "us": {
-                        "value": 2.5,
-                        "unit": "cups"
-                    }
-                }
-            },
-            {
-                "name": "salt",
-                "image": "salt.jpg",
-                "amount": {
-                    "metric": {
-                        "value": 0.333,
-                        "unit": "tsps"
-                    },
-                    "us": {
-                        "value": 0.333,
-                        "unit": "tsps"
-                    }
-                }
-            },
-            {
-                "name": "unsalted butter",
-                "image": "butter-sliced.jpg",
-                "amount": {
-                    "metric": {
-                        "value": 0.5,
-                        "unit": "Tbsps"
-                    },
-                    "us": {
-                        "value": 0.5,
-                        "unit": "Tbsps"
-                    }
+    "vegetarian": true,
+    "vegan": false,
+    "glutenFree": false,
+    "dairyFree": false,
+    "veryHealthy": false,
+    "cheap": false,
+    "veryPopular": false,
+    "sustainable": false,
+    "weightWatcherSmartPoints": 13,
+    "gaps": "no",
+    "lowFodmap": false,
+    "preparationMinutes": 25,
+    "cookingMinutes": 20,
+    "aggregateLikes": 35,
+    "spoonacularScore": 32.0,
+    "healthScore": 3.0,
+    "pricePerServing": 83.17,
+    "extendedIngredients": [
+        {
+            "id": 9003,
+            "aisle": "Produce",
+            "image": "apple.jpg",
+            "consistency": "solid",
+            "name": "apples",
+            "original": "6 medium (6 cups) apples, peeled, cored, sliced",
+            "originalString": "6 medium (6 cups) apples, peeled, cored, sliced",
+            "originalName": "medium apples, peeled, cored, sliced",
+            "amount": 6.0,
+            "unit": "cups",
+            "meta": [
+                "cored",
+                "peeled",
+                "sliced"
+            ],
+            "metaInformation": [
+                "cored",
+                "peeled",
+                "sliced"
+            ],
+            "measures": {
+                "us": {
+                    "amount": 6.0,
+                    "unitShort": "cups",
+                    "unitLong": "cups"
+                },
+                "metric": {
+                    "amount": 1.42,
+                    "unitShort": "l",
+                    "unitLong": "liters"
                 }
             }
-        ]
-    }
+        },
+        {
+            "id": 19334,
+            "aisle": "Baking",
+            "image": "light-brown-sugar.jpg",
+            "consistency": "solid",
+            "name": "brown sugar",
+            "original": "3/4 cup firmly packed brown sugar",
+            "originalString": "3/4 cup firmly packed brown sugar",
+            "originalName": "firmly packed brown sugar",
+            "amount": 0.75,
+            "unit": "cup",
+            "meta": [
+                "packed"
+            ],
+            "metaInformation": [
+                "packed"
+            ],
+            "measures": {
+                "us": {
+                    "amount": 0.75,
+                    "unitShort": "cups",
+                    "unitLong": "cups"
+                },
+                "metric": {
+                    "amount": 177.441,
+                    "unitShort": "ml",
+                    "unitLong": "milliliters"
+                }
+            }
+        },
+        {
+            "id": 93674,
+            "aisle": "Milk, Eggs, Other Dairy",
+            "image": "cinnamon-sugar-butter.png",
+            "consistency": "solid",
+            "name": "cinnamon sugar butter",
+            "original": "1/2 cup Land O Lakes® Cinnamon Sugar Butter Spread",
+            "originalString": "1/2 cup Land O Lakes® Cinnamon Sugar Butter Spread",
+            "originalName": "Land O Lakes® Cinnamon Sugar Butter Spread",
+            "amount": 0.5,
+            "unit": "cup",
+            "meta": [
+                "lakes®"
+            ],
+            "metaInformation": [
+                "lakes®"
+            ],
+            "measures": {
+                "us": {
+                    "amount": 0.5,
+                    "unitShort": "cups",
+                    "unitLong": "cups"
+                },
+                "metric": {
+                    "amount": 118.294,
+                    "unitShort": "ml",
+                    "unitLong": "milliliters"
+                }
+            }
+        },
+        {
+            "id": 20081,
+            "aisle": "Baking",
+            "image": "flour.png",
+            "consistency": "solid",
+            "name": "flour",
+            "original": "1/2 cup all-purpose flour",
+            "originalString": "1/2 cup all-purpose flour",
+            "originalName": "all-purpose flour",
+            "amount": 0.5,
+            "unit": "cup",
+            "meta": [
+                "all-purpose"
+            ],
+            "metaInformation": [
+                "all-purpose"
+            ],
+            "measures": {
+                "us": {
+                    "amount": 0.5,
+                    "unitShort": "cups",
+                    "unitLong": "cups"
+                },
+                "metric": {
+                    "amount": 118.294,
+                    "unitShort": "ml",
+                    "unitLong": "milliliters"
+                }
+            }
+        },
+        {
+            "id": 8120,
+            "aisle": "Cereal",
+            "image": "rolled-oats.jpg",
+            "consistency": "solid",
+            "name": "old-fashioned oats",
+            "original": "3/4 cup uncooked old-fashioned oats",
+            "originalString": "3/4 cup uncooked old-fashioned oats",
+            "originalName": "uncooked old-fashioned oats",
+            "amount": 0.75,
+            "unit": "cup",
+            "meta": [
+                "uncooked"
+            ],
+            "metaInformation": [
+                "uncooked"
+            ],
+            "measures": {
+                "us": {
+                    "amount": 0.75,
+                    "unitShort": "cups",
+                    "unitLong": "cups"
+                },
+                "metric": {
+                    "amount": 177.441,
+                    "unitShort": "ml",
+                    "unitLong": "milliliters"
+                }
+            }
+        }
+    ],
+    "id": 47950,
+    "title": "Cinnamon Apple Crisp",
+    "readyInMinutes": 45,
+    "servings": 6,
+    "sourceUrl": "http://www.landolakes.com/recipe/3168/cinnamon-apple-crisp",
+    "image": "https://spoonacular.com/recipeImages/47950-556x370.jpg",
+    "imageType": "jpg",
+    "summary": "Cinnamon Apple Crisp is a <b>vegetarian</b> dessert. One serving contains <b>347 calories</b>, <b>3g of protein</b>, and <b>10g of fat</b>. This recipe serves 6. For <b>83 cents per serving</b>, this recipe <b>covers 6%</b> of your daily requirements of vitamins and minerals. 35 people have made this recipe and would make it again. Head to the store and pick up flour, firmly brown sugar, land o cinnamon sugar butter spread, and a few other things to make it today. From preparation to the plate, this recipe takes roughly <b>45 minutes</b>. All things considered, we decided this recipe <b>deserves a spoonacular score of 35%</b>. This score is rather bad. Similar recipes include <a href=\"https://spoonacular.com/recipes/cinnamon-apple-crisp-180885\">Cinnamon Apple Crisp</a>, <a href=\"https://spoonacular.com/recipes/moms-cinnamon-apple-crisp-394883\">Mom's Cinnamon-Apple Crisp</a>, and <a href=\"https://spoonacular.com/recipes/cinnamon-apple-crisp-47756\">Cinnamon Apple Crisp</a>.",
+    "cuisines": [],
+    "dishTypes": [
+        "side dish"
+    ],
+    "diets": [
+        "lacto ovo vegetarian"
+    ],
+    "occasions": [],
+    "winePairing": {},
+    "instructions": null,
+    "analyzedInstructions": [],
+    "sourceName": null,
+    "creditsText": null,
+    "originalId": null
+}
