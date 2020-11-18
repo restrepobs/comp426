d72f0c37b51015df770fefa759bf1ba7f19f45dd
Amanda Harris <amandaharris@Amandas-MacBook-Pro-4.local>
2020-11-17 19:30:33 -0500

    dummy login page redirects to registration page

--- venv/app.py
+++ venv/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__, template_folder='templates')
 
@@ -6,12 +6,29 @@ app = Flask(__name__, template_folder='templates')
 # def hello_world():
 #     return 'Hello, World!'
 
-@app.route('/')
+@app.route('/login', methods = ['GET', 'POST'])
 
 def LoginPage():
-    #request_method = request.method
-    return render_template('login.html')
-    #return render_template('login.html', request_method = request_method)
+    request_method = request.method
+    #return render_template('login.html')
+    return render_template('login.html', request_method = request_method)
+
+@app.route('/register', methods = ['GET', 'POST'])
+
+def RegisterPage():
+    request_method = request.method
+    #return render_template('login.html')
+    return render_template('register.html', request_method = request_method)
+    if request.method == 'POST':
+        return redirect('/login')
+
+@app.route('/home', methods = ['GET', 'POST'])
+
+def HomePage():
+    #render the home page
+    return
+    
+
 
 if __name__ == '__main__':
    app.run(debug = True)    
