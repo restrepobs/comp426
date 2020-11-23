18148d5123e1ab28bfe32a1eba65849e3166f6f6
Joseph Cheung <josephcheung630@gmail.com>
2020-11-23 00:43:51 -0500

    browse now shows link to recipes

--- src/pages/Browse.js
+++ src/pages/Browse.js
@@ -4,67 +4,127 @@ import {food} from "../components/Suggestions"
 export class Browse extends Component {
     state = {
         loading: true,
-        title1: null,image1: null,likes1: null,
-        title2: null,image2: null,likes2: null,
-        title3: null,image3: null,likes3: null,
-        title4: null,image4: null,likes4: null,
-        title5: null,image5: null,likes5: null,
-        title6: null,image6: null,likes6: null,
-        title7: null,image7: null,likes7: null,
+        title: null, image: null, website: null,
+        title1: null, image1: null, website1: null,
+        title2: null, image2: null, website2: null,
+        title3: null, image3: null, website3: null,
+        title4: null, image4: null, website4: null,
+        title5: null, image5: null, website5: null,
     }
  
     async componentDidMount() {
-        const randomIndex = Math.floor(Math.random() * food.length);
-        const recipe = food[randomIndex];
-        const url = 'https://api.spoonacular.com/recipes/findByIngredients?apiKey=82fc3eb435d84878928dc33d21b4ded3&ingredients=' + recipe;
+        for (let i = 0; i < 6; i++) {
+
+        const randomNumber = Math.floor(Math.random() * 100000);
+        const url = 'https://api.spoonacular.com/recipes/' + randomNumber + '/information?includeNutrition=false&apiKey=35d14b0f986b4ce68a582bf645a7825e'
         const response = await fetch(url);
         const data = await response.json();
+        console.log(i + data.id)
+        if (i === 0) {
+            this.setState({loading: false,
+                title: data.title,
+                image: data.image,
+                website: data.sourceUrl,
+            })
+        } else if (i === 1) {
+            this.setState({loading: false,
+                title1: data.title,
+                image1: data.image,
+                website1: data.sourceUrl,
+            })
+        } else if (i === 2){
+            this.setState({loading: false,
+                title2: data.title,
+                image2: data.image,
+                website2: data.sourceUrl,
+            })
+        } else if (i === 3) {
+            this.setState({loading: false,
+                title3: data.title,
+                image3: data.image,
+                website3: data.sourceUrl,
+            })
+        } else if (i === 4) {
+            this.setState({loading: false,
+                title4: data.title,
+                image4: data.image,
+                website4: data.sourceUrl,
+            })
+        } else {
+            this.setState({loading: false,
+                title5: data.title,
+                image5: data.image,
+                website5: data.sourceUrl,
+            })
+        }
+       
 
-        this.setState({loading: false,
-        title1: data[0].title, image1: data[0].image, likes1: data[0].likes,
-        title2: data[1].title, image2: data[1].image, likes2: data[1].likes,
-        title3: data[2].title, image3: data[2].image, likes3: data[2].likes,
-        title4: data[3].title, image4: data[3].image, likes4: data[3].likes,
-        title5: data[4].title, image5: data[4].image, likes5: data[4].likes,
-        title6: data[5].title, image6: data[5].image, likes6: data[5].likes,
-        title7: data[6].title, image7: data[6].image, likes7: data[6].likes,
-        })
+        }
     }
 
     refreshPage = () => {
         this.componentDidMount();
     };
+    
   
     render() {
         return ( 
             <div>
             <br></br>
-            <br></br>
-            <br></br>
             <div>
-                <p>Don't have any recipes in mind? Browse through these recipes and see which ones you like best!</p>
-                <button class="btn btn-primary my-3" onClick={this.refreshPage}>Generate New Foods To Look At</button>
-                <div>{this.state.title1}</div>
-                <div>Likes: {this.state.likes1}</div>
-                <img src={this.state.image1}/>
-                <div>{this.state.title2}</div>
-                <div>Likes: {this.state.likes2}</div>
-                <img src={this.state.image2}/>
-                <div>{this.state.title3}</div>
-                <div>Likes: {this.state.likes3}</div>
-                <img src={this.state.image3}/>
-                <div>{this.state.title4}</div>
-                <div>Likes: {this.state.likes4}</div>
-                <img src={this.state.image4}/>
-                <div>{this.state.title5}</div>
-                <div>Likes: {this.state.likes5}</div>
-                <img src={this.state.image5}/>
-                <div>{this.state.title6}</div>
-                <div>Likes: {this.state.likes6}</div>
-                <img src={this.state.image6}/>
-                <div>{this.state.title7}</div>
-                <div>Likes: {this.state.likes7}</div>
-                <img src={this.state.image7}/>
+            <h2>Can't think of any recipes? That's okay! Browse through these recipes and see which ones you like.</h2>
+            
+            <br></br>
+
+            <div style={{fontSize:'20px'}}>{this.state.title}</div>  
+            <img src={this.state.image}/>   
+            <br></br>
+            <a href={this.state.website} target="_blank" rel="noopener noreferrer" >recipe link</a>
+
+            <br></br>
+            <br></br>
+
+            <div style={{fontSize:'20px'}}>{this.state.title1}</div>  
+            <img src={this.state.image1}/>   
+            <br></br>
+            <a href={this.state.website1} target="_blank" rel="noopener noreferrer" >recipe link</a>
+            
+            <br></br>
+            <br></br>
+
+            <div style={{fontSize:'20px'}}>{this.state.title2}</div>  
+            <img src={this.state.image2}/>   
+            <br></br>
+            <a href={this.state.website2} target="_blank" rel="noopener noreferrer" >recipe link</a>
+            
+            <br></br>
+            <br></br>
+
+            <div style={{fontSize:'20px'}}>{this.state.title3}</div>  
+            <img src={this.state.image3}/>   
+            <br></br>
+            <a href={this.state.website3} target="_blank" rel="noopener noreferrer" >recipe link</a>
+
+            <br></br>
+            <br></br>
+
+            <div style={{fontSize:'20px'}}>{this.state.title4}</div>  
+            <img src={this.state.image4}/>   
+            <br></br>
+            <a href={this.state.website4} target="_blank" rel="noopener noreferrer" >recipe link</a>
+
+            <br></br>
+            <br></br>
+
+            <div style={{fontSize:'20px'}}>{this.state.title5}</div>  
+            <img src={this.state.image5}/>   
+            <br></br>
+            <a href={this.state.website5} target="_blank" rel="noopener noreferrer" >recipe link</a>
+
+            <br></br>
+            <br></br>
+
+            <button className='btn btn-info my-3' onClick={this.refreshPage}>Generate More Recipes</button>
             </div>
         </div>
         );
@@ -72,3 +132,4 @@ export class Browse extends Component {
 }
 
 export default Browse;
+
